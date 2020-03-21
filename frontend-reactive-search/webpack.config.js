e7925ca6eed5891327bf7a3a62a974bb86e53225
programmieraffe <info@matthias-andrasch.de>
2020-03-21 13:52:54 +0100

    add react and build it to docs

---
+++ frontend-reactive-search/webpack.config.js
@@ -0,0 +1,60 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+/* EXPERIMENTAL: BUILD TO docs/ so we can use it in gitlab pages */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path:__dirname+'/../docs',
+    /*path: __dirname + 'public/',*/
+    /* BEWARE DELETES FOLDER!!!! path: __dirname +'/../web/assets/js',*/
+    /*publicPath: '/',*/
+    filename: 'oerhoernchen.higher_education.react.js'
+  },
+  devServer: {
+    contentBase: './',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+       	use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+      }
+    ]
+  },
+  plugins:[new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: 'public/index.html', to: 'index.html' },
+    ]),
+    new FileManagerPlugin({
+      onEnd: {
+        copy: [
+          { source: 'build/oerhoernchen.higher_education.react.js', destination: '../web/assets/js' }
+        ]
+        }
+      })
+  ]
+  /*,
+   plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            filename: 'index.html' //relative to root of the application
+        })
+   ]*/
+};
